a91c706aa9eb31a93812bbb093d22716f0535555
ruppal <raheeluppal323@gmail.com>
2020-04-29 12:50:30 -0400

    working on service/repo methods

--- src/main/java/NBA/API/repositories/PlayerListRepository.java
+++ src/main/java/NBA/API/repositories/PlayerListRepository.java
@@ -1,9 +1,16 @@
 package NBA.API.repositories;
 
+import NBA.API.models.Player;
 import NBA.API.models.PlayerList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PlayerListRepository extends JpaRepository<PlayerList, Long> {
+
+    @Query("SELECT t FROM Player WHERE t.team_id = : team_is")
+    Long findByTeamId(Player player);
 }
--- src/main/java/NBA/API/services/PlayerListService.java
+++ src/main/java/NBA/API/services/PlayerListService.java
@@ -1,10 +1,15 @@
 package NBA.API.services;
 
+import NBA.API.models.Player;
 import NBA.API.models.PlayerList;
 import NBA.API.models.Team;
 import NBA.API.repositories.PlayerListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlayerListService {
@@ -16,12 +21,20 @@ public class PlayerListService {
         this.repo = repo;
     }
 
-    public PlayerList saveByTeamId(PlayerList playerList, Team team){
+    public PlayerList saveByTeamId(PlayerList playerList){
+//        List<Player> plData = playerList.getData();
+//        for(Player p : plData){
+//            p.getTeam().getId();
+//        }
         return repo.save(playerList);
     }
 
-    public PlayerList findByTeamId(){
-
-        return null;
+    public Optional<Player> findByTeamId(Player player){
+//        List<Player> plData = playerList.getData();
+//        for(Player p : plData){
+//            p.getTeam().getId();
+//        }
+//        return repo.findByTeamId(player.getTeam().getId());
+        return Optional.empty();
     }
 }
